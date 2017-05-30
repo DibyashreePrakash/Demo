61891183b9596753f32668c046ec107db2ef7b81
dibya904047 <dibyashree.prakash@infostretch.com>
2017-05-30 12:14:55 +0530

    by tester

--- FlipkartMobileApp/src/test/java/com/assignment/android/flipkartmobileapp/pages/HomeTestPage.java
+++ FlipkartMobileApp/src/test/java/com/assignment/android/flipkartmobileapp/pages/HomeTestPage.java
@@ -15,6 +15,7 @@ public class HomeTestPage extends WebDriverBaseTestPage<WebDriverTestPage> {
 
 	//Changes 
 	//By Dibya
+//By testengg
 	@FindBy(locator = "btn.menu.flipkart")
 	private QAFWebElement btnMenuFlipkart;
 	@FindBy(locator = "btn.Electronics.flipkart")
